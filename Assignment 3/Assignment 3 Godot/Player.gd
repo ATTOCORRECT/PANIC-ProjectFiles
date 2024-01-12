876acb9590c0ed38586c5d3c4ea66ba13aa59105
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2024-01-11 19:02:18 -0500

    things

--- Assignment 3/Assignment 3 Godot/Player.gd
+++ Assignment 3/Assignment 3 Godot/Player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const MAX_SPEED = 500.0 # units per tick
+const ACCELERATION = 50.0 # units per tick ^2
+const FRICTION = 30.0 # units per tick ^2
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,15 +15,16 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+
 
 	move_and_slide()
