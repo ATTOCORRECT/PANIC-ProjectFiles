49ae7776d51a91acc37e07098959b507f686a279
ATTOCORRECT <97545662+ATTOCORRECT@users.noreply.github.com>
2024-02-07 12:13:14 -0500

    Wall jumping

--- Assignment 3/Assignment 3 Godot/Scripts/Player.gd
+++ Assignment 3/Assignment 3 Godot/Scripts/Player.gd
@@ -2,13 +2,21 @@ extends CharacterBody2D
 
 # constants
 const MAX_SPEED = 500.0 # units per tick
+const MAX_WALK_SPEED = 200.0 # units per tick
 const ACCELERATION = 5000.0 # units per tick ^2
 const FRICTION = 3000.0 # units per tick ^2
-const JUMP_VELOCITY = -750.0
+const WALL_FRICTION = 2000.0 # units per tick ^2
+const JUMP_VELOCITY = 750.0
 const SWING_SPEED = 700.0
 
+var direction = 0
+
+# bools
 var canSwing = false
 var timeSlow = false
+var canMove = true
+var isOnCoyoteFloor = true
+var isOnCoyoteWallOnly = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -23,8 +31,12 @@ func _physics_process(delta): # physics update
 		velocity.y += gravity * delta
 	
 	sword()
+	coyoteFloor()
 	jump(delta)
 	move(delta)
+	coyoteWall()
+	wallJump()
+	#wallSlide(delta)
 	move_and_slide()
 	queue_redraw()
 
@@ -47,12 +59,50 @@ func sword():
 		canSwing = true
 
 func jump(delta): # Handle Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and isOnCoyoteFloor:
+		isOnCoyoteFloor = false
+		velocity += Vector2.UP * JUMP_VELOCITY
+
+func coyoteFloor():
+	if is_on_floor():
+		isOnCoyoteFloor = true
+		$CoyoteTimer.start()
+	
+	if $CoyoteTimer.is_stopped():
+		isOnCoyoteFloor = false
+
+func wallJump():
+	if Input.is_action_just_pressed("jump") and isOnCoyoteWallOnly:
+		isOnCoyoteWallOnly = false
+		velocity += (Vector2.UP + Vector2.RIGHT * get_wall_normal().x).normalized() * JUMP_VELOCITY
+		
+		canMove = false
+		$WallJumpInputSupresstionTimer.start()
+	
+	if $WallJumpInputSupresstionTimer.is_stopped():
+		canMove = true
+
+func coyoteWall():
+	if is_on_wall_only():
+		isOnCoyoteWallOnly = true
+		$WallCoyoteTimer.start()
+	
+	if is_on_floor():
+		isOnCoyoteWallOnly = false
+	
+	if $WallCoyoteTimer.is_stopped():
+		isOnCoyoteWallOnly = false
+
+func wallSlide(delta):
+	if is_on_wall_only() and get_wall_normal().x * direction < 0:
+		velocity.y = velocity.y * 10 * delta * abs(get_wall_normal().x)
 
 func move(delta): # Get the input direction and handle the movement/deceleration
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction and velocity.x * direction < MAX_SPEED:
+	direction = Input.get_axis("move_left", "move_right")
+	if direction and velocity.x * direction < MAX_SPEED and canMove:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+
+
