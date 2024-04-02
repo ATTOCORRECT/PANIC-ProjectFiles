1bbed267a64833eb727e4091083f355c93615541
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2024-04-02 15:09:56 -0400

    pause menu

--- Assignment 3 Godot/Scripts/world.gd
+++ Assignment 3 Godot/Scripts/world.gd
@@ -5,18 +5,32 @@ var isPaused = false;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	
+	if Telemetry.has_authenticated():
+		_on_authenticated(true, Telemetry.get_session_id())
+
+func _on_authenticated(success, info):
+	if success:
+		if info < 0:
+			$CanvasLayer/SessionInfo.text = "Offline Session #" + str(info)
+		else:
+			$CanvasLayer/SessionInfo.text = "Server Session #" + str(info)
+	else:
+		$CanvasLayer/SessionInfo.text = str(info)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	PauseMenu()
-	pass
-	
+
 func PauseMenu():
 	if Input.is_action_just_pressed("ui_cancel"):
 		if get_tree().paused: # unpause the game
 			get_tree().paused = false
 			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+			$CanvasLayer/PauseMenu.visible = false
 			
 		elif not get_tree().paused: # pause the game
 			get_tree().paused = true
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			$CanvasLayer/PauseMenu.visible = true
+			
